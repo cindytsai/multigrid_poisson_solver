76bcb8c31c2d93e5fb937c8f0adfb69d9de1fa4b
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-05-23 21:59:54 +0800

    Initialize MG_solver_CPU.cpp

---
+++ src/MG_solver_CPU.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstdlib>
+#include <omp.h>
+#include <math.h>
+#include <cstring>
+
+/*
+For problem to be solved
+ */
+
+/*
+MultiGrid Functions
+ */
+void getResidual(int, double*, double*, double*);
+void doSmoothing(int, double*, double*, int);
+void doExactSolver(int, double*, double*, double);
+void doRestriction(int, double*, int, double*);
+void doProlongation(int, double*, int, double*);
+
+
+int main(){
+	/*
+	Settings
+	cycle: The cycle
+	N:     Grid size
+	 */
+	int cycle[5] = {-1, -1, 0, 1, 1};
+	int N;
+	int L;
+	
+	/*
+	Main MultiGrid Solver
+	 */
+	// for(int i = 0; i < cycle.length; i = i+1){
+
+	// }
+
+	return 0;
+}
+
+
+/*
+Functions
+ */
