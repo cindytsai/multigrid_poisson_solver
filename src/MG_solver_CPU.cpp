8cbd01797395b4e5269c3227b61fc93d0dbb6c8e
Hsu Chin-Hao <esther5435@gmail.com>
2020-05-28 18:42:38 +0800

    add getResidual and doSmoothing

--- src/MG_solver_CPU.cpp
+++ src/MG_solver_CPU.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <omp.h>
 #include <math.h>
 #include <cstring>
@@ -43,13 +45,14 @@ void doProlongation(int, double*, int, double*);
 	//return 0;
 //}
 
-
+/*
 
 int main(){
 	int N=3, M=5;
 	double *U_f, *U_c;
 	U_f = (double *)malloc(M*M*sizeof(double));
 	U_c = (double *)malloc(N*N*sizeof(double));
+
 
 	for(int i=0; i<N; i++){
 		for(int j=0; j<N; j++){
@@ -69,10 +72,50 @@ int main(){
 
 }
 
+*/
+
+int main(){
+	int N=16;
+	double L=1.0;
+	double *U, *F, *D;
+	F = (double *)malloc(N*N*sizeof(double));
+	U = (double *)malloc(N*N*sizeof(double));
+	D = (double *)malloc(N*N*sizeof(double));
+
+	getSource(N, L, F, 0.0, 0.0);
+	getResidual(N, L, U, F, D);
+	doSmoothing(N,  L, U, F, 100);
+
+
+
+	FILE *file1;
+
+    file1 = fopen("residual", "w");
+
+  	for (int i = 0; i < N; i++) {
+    	for (int j = 0; j < N; j++) {
+      		if (j != N-1) {fprintf(file1, "%f,", *(D + i*N + j));}
+      		else {fprintf(file1, "%f\n", *(D + i*N + j));}
+    	}
+  	}
+
+  	FILE *file2;
+
+    file2 = fopen("solution", "w");
+
+  	for (int i = 0; i < N; i++) {
+    	for (int j = 0; j < N; j++) {
+      		if (j != N-1) {fprintf(file2, "%f,", *(U + i*N + j));}
+      		else {fprintf(file2, "%f\n", *(U + i*N + j));}
+      
+    	}
+  	}
+}
+
 
 void doProlongation(int N, double* U_c, int M, double* U_f){
 	double L = 1.0;
-	double c_dx = L / (N-1), f_dx = L/(M-1);
+	double c_dx = L / (double) (N-1), f_dx = L/(double) (M-1);
 	double ratio = c_dx/f_dx;
 
 	for(int i=0; i<N-1; i++){
@@ -124,12 +167,53 @@ void doProlongation(int N, double* U_c, int M, double* U_f){
 			
 		}
 	}
+}
 
+void doSmoothing(int N, double L, double* U, double* F, int step){
+	double dx=L/(double) (N-1);
+	double delta;
+	//Gauss-Seidel
+	for(int s=0; s<step; s++){
+		for(int i=1; i<N-1; i++){
+			for(int j=1; j<N-1; j++){
+				delta = 0.25*( *(U+(i+1)*N+j) + *(U+(i-1)*N+j) + *(U+i*N+(j+1)) + *(U+i*N+(j-1)) - 4* *(U+i*N+j) - pow(dx,2) * *(F+i*N+j));
+		        *(U+i*N+j) += delta;
+			}
+		}
+	}
+	
+}
 
-
+void getResidual(int N, double L, double* U, double* F, double* D){
+	double dx=L/(double) (N-1);
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if (i==0 or j==0 or j==N-1)  *(D+i*N+j)=0.0;
+			else  *(D+i*N+j) =  *(U+(i+1)*N+j) + *(U+(i-1)*N+j) + *(U+i*N+(j+1)) + *(U+i*N+(j-1)) - 4* *(U+i*N+j) - pow(dx,2) * *(F+i*N+j);
+		}
+	}
 
 }
 
+
+// Get the source f of the problem Lu = f
+void getSource(int N, double L, double *F, double min_x, double min_y){
+
+	double h = L / (double) (N-1);
+	double x, y;
+	int index;
+
+	for(int iy = 0; iy < N; iy = iy+1){
+		for(int ix = 0; ix < N; ix = ix+1){
+			index = ix + N * iy;
+			x = (double)ix * h + min_x;
+			y = (double)iy * h + min_y;
+
+			// Source from the problem
+			F[index] = 2.0 * x * (y - 1) * (y - 2.0 * x + x * y + 2.0) * exp(x - y);
+		}
+	}
+}
 /*
 Functions
  */
