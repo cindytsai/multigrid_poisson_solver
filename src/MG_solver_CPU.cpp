46a89e3548cf32123d559f8ed9070db0613777fc
Hsu Chin-Hao <esther5435@gmail.com>
2020-05-29 10:40:20 +0800

    minor bugs fixed(definition of residual)

--- src/MG_solver_CPU.cpp
+++ src/MG_solver_CPU.cpp
@@ -100,8 +100,8 @@ void getResidual(int N, double L, double* U, double* F, double* D){
 	double dx=L/(double) (N-1);
 	for(int i=0; i<N; i++){
 		for(int j=0; j<N; j++){
-			if (i==0 or j==0 or j==N-1)  *(D+i*N+j)=0.0;
-			else  *(D+i*N+j) =  *(U+(i+1)*N+j) + *(U+(i-1)*N+j) + *(U+i*N+(j+1)) + *(U+i*N+(j-1)) - 4* *(U+i*N+j) - pow(dx,2) * *(F+i*N+j);
+			if (i==0 or j==0 or j==N-1 or i==N-1)  *(D+i*N+j)=0.0;
+			else  *(D+i*N+j) =  (1.0/pow(dx,2)) * (*(U+(i+1)*N+j) + *(U+(i-1)*N+j) + *(U+i*N+(j+1)) + *(U+i*N+(j-1)) - 4* *(U+i*N+j)) - *(F+i*N+j);
 		}
 	}
 
